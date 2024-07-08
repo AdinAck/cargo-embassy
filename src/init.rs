edf6c7ef774837c476ddcd9e174577a4a8ffc3c6
Adin Ackerman <adinackerman@gmail.com>
2024-07-08 08:23:17 -0700

    fix build errors, fix leaked SD message

--- src/init.rs
+++ src/init.rs
@@ -1,9 +1,5 @@
 use crate::{
-    chip::{
-        family::{mem_region::MemRegion, Family},
-        target::Target,
-        Chip,
-    },
+    chip::{family::Family, target::Target, Chip},
     cli::init_args::{panic_handler::PanicHandler, soft_device::Softdevice, InitArgs},
     error::{Error, InvalidChip},
 };
@@ -65,12 +61,10 @@ impl Init {
         )?;
         self.init_fmt()?;
         self.init_main(&chip.family, &args.panic_handler, args.softdevice.as_ref())?;
+        self.init_memory_x(&chip.family)?;
 
-        if let Family::NRF(mem_reg) = chip.family {
-            self.init_memory_x(mem_reg)?;
+        if args.softdevice.is_some() {
             self.pb.println("[ACTION NEEDED] You must now flash the Softdevice and configure memory.x. Instructions can be found here: https://github.com/embassy-rs/nrf-softdevice#running-examples.");
-        } else if let Family::RP2040 = chip.family {
-            self.init_memory_x_rp()?;
         }
 
         Ok(())
@@ -163,15 +157,14 @@ impl Init {
         self.cargo_add("embassy-sync", None, false)?;
         self.cargo_add("embassy-futures", None, false)?;
 
-        
-
         self.cargo_add(
             "embassy-time",
             match chip.family {
                 Family::RP2040 => None,
-                _ => Some(&["tick-hz-32_768"])
+                _ => Some(&["tick-hz-32_768"]),
             },
-            false)?;
+            false,
+        )?;
 
         match chip.family {
             Family::STM32 => self.cargo_add(
@@ -200,7 +193,7 @@ impl Init {
                 self.cargo_add("embassy-embedded-hal", None, false)?;
 
                 Ok(())
-            },
+            }
         }?;
 
         if let Some(softdevice) = softdevice {
@@ -311,24 +304,23 @@ impl Init {
         )
     }
 
-    fn init_memory_x(&self, memory: MemRegion) -> Result<(), Error> {
-        self.create_file(
-            "memory.x",
-            &format!(
-                include_str!("templates/memory.x.template"),
-                flash_origin = memory.flash_origin,
-                flash_len = memory.flash_length,
-                ram_origin = memory.ram_origin,
-                ram_len = memory.ram_length,
+    fn init_memory_x(&self, family: &Family) -> Result<(), Error> {
+        match family {
+            Family::NRF(mem) => self.create_file(
+                "memory.x",
+                &format!(
+                    include_str!("templates/memory.x.nrf.template"),
+                    flash_origin = mem.flash_origin,
+                    flash_len = mem.flash_length,
+                    ram_origin = mem.ram_origin,
+                    ram_len = mem.ram_length,
+                ),
             ),
-        )
-    }
-
-    fn init_memory_x_rp(&self) -> Result<(), Error> {
-        self.create_file(
-            "memory.x",
-            &include_str!("templates/memory.x.rp.template"),
-        )
+            Family::RP2040 => {
+                self.create_file("memory.x", &include_str!("templates/memory.x.rp.template"))
+            }
+            _ => Ok(()),
+        }
     }
 
     fn create_file(&self, name: &str, content: &str) -> Result<(), Error> {
